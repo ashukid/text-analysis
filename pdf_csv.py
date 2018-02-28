73274abeea083f7901fcd088b1362b9cf4cdd4dc
ashukid <irm2015001@iiita.ac.in>
2018-02-28 23:40:40 +0530

    feature extraction started

---
+++ pdf_csv.py
@@ -0,0 +1,28 @@
+import nltk
+import PyPDF2 
+import re
+import pandas as pd
+
+pdf_file = open('test.pdf','rb') 
+read_pdf = PyPDF2.PdfFileReader(pdf_file) 
+num_pages = read_pdf.getNumPages()
+count = 3 #the starting page
+text = ""
+#The while loop will read each page
+while count < 25:
+	pageObj = read_pdf.getPage(count) 
+	count +=1
+	text += pageObj.extractText()
+pdf_file.close()
+
+text=re.sub('\\n', '', text)
+
+from nltk.tokenize import sent_tokenize 
+sents=sent_tokenize (text)
+
+for i in range(len(sents)):
+	sents[i] = sents[i].encode('utf-8')
+
+test = pd.DataFrame()
+test['sentence'] = sents
+test.to_csv("test.csv",index_label=False)
