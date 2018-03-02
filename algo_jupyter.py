3bec91980c4238e356c905dc615584be16c05f5a
ashukid <irm2015001@iiita.ac.in>
2018-03-02 11:02:11 +0530

    done implementing classifiers

--- algo_jupyter.py
+++ algo_jupyter.py
@@ -11,8 +11,9 @@ from nltk.corpus import stopwords
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import VotingClassifier
 from sklearn.feature_extraction.text import TfidfVectorizer
-
+import re
 
 
 train = pd.read_csv('train.csv')
@@ -23,15 +24,12 @@ y_train = np.ravel(train["Condition"])
 
 
 
-train.count()
-
-
-
 def data_preprocessing(dataset):
     new_dataset = []
     stop_words = set(stopwords.words('english'))
     for i in range(len(dataset)):
         sent = dataset.loc[i,["Statment"]].values[0].decode('utf-8')
+        sent = re.sub('[^a-zA-Z]', ' ', sent)
         sent_token = sent.lower().split()
         word_token = [word for word in sent_token if not word in stop_words]
         stem = SnowballStemmer("english")
@@ -46,22 +44,52 @@ x_train = data_preprocessing(train)
 x_test = data_preprocessing(test)
 
 
-
 vectorizer=TfidfVectorizer(stop_words='english')
 train_tfidf=vectorizer.fit_transform(x_train)
-train_tfidf=train_tfidf.toarray()
-clf=RandomForestClassifier(max_depth=20,random_state=80)
-eq=clf.fit(train_tfidf,y_train)
-
-
-
+train_tfidf_array=train_tfidf.toarray()
 test_tfidf=vectorizer.transform(x_test)
 test_tfidf_array=test_tfidf.toarray()
+
+
+# Random Forest Classifier
+clf_rf=RandomForestClassifier(max_depth=20,random_state=80)
+clf_rf.fit(train_tfidf_array,y_train)
+prediction_rf = clf_rf.predict(test_tfidf_array)
+print("Random Forest Accuracy : {}".format(clf_rf.score(train_tfidf_array,y_train)))
+
+
+
+# naive bayes classifier
+clf_nb=MultinomialNB()
+clf_nb.fit(train_tfidf_array,y_train)
+prediction_nb = clf_nb.predict(test_tfidf_array)
+print("Multinomail Naive Bayes Accuracy : {}".format(clf_nb.score(train_tfidf_array,y_train)))
+
+
+# Decision tree classifier
+clf_dt=DecisionTreeClassifier()
+clf_dt.fit(train_tfidf_array,y_train)
+prediction_dt = clf_dt.predict(test_tfidf_array)
+print("Decision Tree Accuracy : {}".format(clf_dt.score(train_tfidf_array,y_train)))
+
+
+# Voting classifier
+# this classifier just uses 2 or more classifier to predict
+# and then takes the majority vote
+clf = VotingClassifier(estimators=[('rf',clf_rf),('dt',clf_dt),('nb',clf_nb)],voting='soft')
+clf.fit(train_tfidf_array,y_train)
+prediction = clf.predict(test_tfidf_array)
+print("Voting Classifier Accuray : {}".format(clf.score(train_tfidf_array,y_train)))
+
+
 # print(test_tfidf_array)
-prediction = eq.predict(test_tfidf_array)
 output = pd.DataFrame()
 output["Statment"] = test["Statment"]
-output["condition"] = prediction
-# print(output['condition'].value_counts())
-print(output.head())
 
+# Decision tree as final classifer
+# as it gave maximum accuracy
+output["condition"] = prediction_dt
+output['condition'].value_counts()
+
+# final prediction is saved in output.csv
+output.to_csv('output.csv')
